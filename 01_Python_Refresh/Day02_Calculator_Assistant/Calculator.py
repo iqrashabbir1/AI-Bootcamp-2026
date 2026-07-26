ae827f0e35c8f2ea25e355059ab60438bf413f0c
Asim <masim.amin@yahoo.com>
2026-07-26 12:06:31 +0200

    Day 2: Built Smart Calculator Assistant

---
+++ 01_Python_Refresh/Day02_Calculator_Assistant/Calculator.py
@@ -0,0 +1,41 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+
+print("=== Smart Calculator Assistant ===")
+
+num1 = float(input("Enter first number: "))
+operator = input("Choose operation (+,-,*,/): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = add(num1, num2)
+
+elif operator == "-":
+    result = subtract(num1, num2)
+
+elif operator == "*":
+    result = multiply(num1, num2)
+
+elif operator == "/":
+    result = divide(num1, num2)
+
+else:
+    result = "Invalid operation"
+
+
+print("Result:", result)
